89796e2ee1a03a973e68aeb5f87034e9e4ebbef7
neeraj <neeraj@neeraj>
2024-07-15 22:43:05 +0530

    api working

--- src/main/java/com/cbt/split/user/api/UsersApi.java
+++ src/main/java/com/cbt/split/user/api/UsersApi.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -20,6 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
  * @author neeraj
  */
 @RestController
+@RequestMapping("/user")
+//@CrossOrigin(origins = "http://localhost:4200")
 public class UsersApi {
 
     @Autowired
@@ -29,7 +32,7 @@ public class UsersApi {
 //    public UsersApi(UserWritePlatformService userWritePlatformService) {
 //        this.userWritePlatformService = userWritePlatformService;
 //    }
-    @PostMapping("/user")
+    @PostMapping("/setUser")
     public ResponseEntity<UserEntity> createUser(@RequestBody UserEntity userData) {
         try {
             UserEntity user = this.userWritePlatformService.createUser(userData);
@@ -46,12 +49,14 @@ public class UsersApi {
 
     @GetMapping()
     public String getAllUser() {
-        return "Get Users Successfully...";
+        System.out.println("Only get user");
+        return "Get Users Successfully...1";
     }
 
     @GetMapping("/userId")
     public String getUser() {
-        return "Get User Successfully...";
+        System.out.println("Hi This Is My Api");
+        return "Get User Successfully... Hiiii";
     }
 
 }
